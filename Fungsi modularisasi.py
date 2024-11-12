10e940aeef7b9146f2344d74574f351b79419a23
Alda952 <aldasyafitri2006@gmail.com>
2024-11-13 00:41:40 +0800

    Create Fungsi modularisasi.py

---
+++ Fungsi modularisasi.py
@@ -0,0 +1,14 @@
+# fungsi dan modularisasi.py
+
+def fungsi():
+    print("Menghitung Luas Segitiga")
+    
+    # Input dari pengguna
+    alas = float(input("Masukkan panjang alas segitiga: "))
+    tinggi = float(input("Masukkan tinggi segitiga: "))
+    
+    # Menghitung luas segitiga
+    
+    
+    # Menampilkan hasil
+    print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah: {luas}")
